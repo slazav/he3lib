d67fd4031b3bb631738511489e28f0e907b3f7cd
Vladislav Zavjalov <slazav@altlinux.org>
2013-11-26 22:48:55 +0200

    he3_text.f: fix some NaN values at T=Tc

--- he3_text.f
+++ he3_text.f
@@ -7,6 +7,8 @@
         include 'he3.fh'
         real*8 ttc,p
         real*8 gd,gap,f0a,z3,z5,y0
+
+
         gd  = he3_gd(p)
         gap = he3_trivgap(ttc,p)
         f0a = he3_f0a(p)
@@ -14,9 +16,15 @@
         z5  = he3_z5(ttc,gap)
         y0  = 1-z3
 
-        he3_text_a = 2.5D0*gd
-     .   * (0.5D0*const_hbar*he3_gyro/(1D0+f0a*(2D0+y0)/3D0))**2
-     .   * (5D0-3D0*z5/z3)
+        he3_text_a = NaN
+        if (ttc.ge.0D0.and.ttc.lt.1D0) then
+          he3_text_a = 2.5D0*gd
+     .     * (0.5D0*const_hbar*he3_gyro/(1D0+f0a*(2D0+y0)/3D0))**2
+     .     * (5D0-3D0*z5/z3)
+        elseif (ttc.eq.1D0) then
+          he3_text_a = 2.5D0*gd
+     .     * 5D0 * (0.5D0*const_hbar*he3_gyro/(1D0+f0a))**2
+        endif
       end
 
 ! Textural parameter lambda_{DV}, erg/cm^3 1/(cm/s)^2
@@ -33,9 +41,15 @@
         z5  = he3_z5(ttc,gap)
         y0  = 1-z3
 
-        he3_text_ldv = 5D0*gd
-     .   * ( he3_meff(p) *he3_vf(p) / (1D0 + f1s*y0/3D0))**2
-     .   * (1D0-1.5D0*z5/z3)
+        he3_text_ldv=NaN
+        if (ttc.ge.0D0.and.ttc.lt.1D0) then
+          he3_text_ldv = 5D0*gd
+     .     * ( he3_meff(p) *he3_vf(p) / (1D0 + f1s*y0/3D0))**2
+     .     * (1D0-1.5D0*z5/z3)
+        elseif (ttc.eq.1D0) then
+          he3_text_ldv = 5D0*gd
+     .     * ( he3_meff(p) *he3_vf(p) / (1D0 + f1s/3D0))**2
+        endif
       end
 
 ! Textural parameter lambda_{HV}, g/(cm^3 Gauss^2)
@@ -52,14 +66,22 @@
         z3  = he3_z3(ttc,gap)
         z5  = he3_z5(ttc,gap)
         z7  = he3_z7(ttc,gap)
-
         gape = gap*const_kb*1D-3*he3_tc(p)
-        he3_text_lhv = he3_rho(p) / gape**2
-     .   * (1D0 + f1s/3D0) / (1D0 + f1s*Y0/3D0)**2
-     .   * (0.5D0*const_hbar*he3_gyro /
-     .          (1D0 + f0a*(2D0 + Y0)/3D0))**2
-     .   * (z3 - 0.9D0*z5 + 0.9D0 * z5**2/z3 - 1.5D0*z7)
 
+        he3_text_lhv=NaN
+        if (ttc.ge.0D0.and.ttc.lt.1D0) then
+          he3_text_lhv = he3_rho(p) / gape**2
+     .     * (1D0 + f1s/3D0) / (1D0 + f1s*Y0/3D0)**2
+     .     * (0.5D0*const_hbar*he3_gyro /
+     .            (1D0 + f0a*(2D0 + Y0)/3D0))**2
+     .     * (z3 - 0.9D0*z5 + 0.9D0 * z5**2/z3 - 1.5D0*z7)
+        elseif (ttc.eq.1D0) then
+          he3_text_lhv = he3_rho(p)
+     .     / (1D0 + f1s/3D0)
+     .     * (0.5D0*const_hbar*he3_gyro / (1D0 + f0a))**2
+     .     / (const_kb*1D-3*he3_tc(p))**2 * 2D0/3D0/const_pi
+           ! note: z3/gap^2 = 2/3/pi at ttc=1
+        endif
         ! negative values can appear because of inaccurate
         ! z3,z5,z7 at low temperatures
         if (he3_text_lhv.lt.0D0) he3_text_lhv=0D0
@@ -100,17 +122,22 @@
         implicit none
         include 'he3.fh'
         real*8 ttc,p
-
         real*8 gap,y0,f0a,n0,xi,d0
-        gap = he3_trivgap(ttc,p)
-        y0  = he3_yosida(ttc,gap,0)
-        f0a = he3_f0a(p)
-        n0  = he3_2n0(p)/2D0
-        xi  = he3_xigl(ttc,p)
-        d0  = 2.2D0 + p*0.5D0/34.39D0
-        he3_text_d = n0*(const_hbar*he3_gyro)**2
-     .   * xi*d0*(1-y0)
-     .   / (4D0*(1D0+f0a)*(3D0+f0a*(2D0+y0)))
+
+        he3_text_d = NaN
+        if (ttc.ge.0D0.and.ttc.lt.1D0) then
+          gap = he3_trivgap(ttc,p)
+          y0  = he3_yosida(ttc,gap,0)
+          f0a = he3_f0a(p)
+          n0  = he3_2n0(p)/2D0
+          xi  = he3_xigl(ttc,p)
+          d0  = 2.2D0 + p*0.5D0/34.39D0
+          he3_text_d = n0*(const_hbar*he3_gyro)**2
+     .     * xi*d0*(1-y0)
+     .     / (4D0*(1D0+f0a)*(3D0+f0a*(2D0+y0)))
+        elseif (ttc.eq.1D0) then
+          he3_text_d = 0D0
+        endif
       end
 
 ! Vortex enrgy coefficient \lambda_{LH}
