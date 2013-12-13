3dc175ea801b24624d7472f51eddadcacdcd832f
Vladislav Zavjalov <slazav@altlinux.org>
2013-12-13 13:45:53 +0000

    test_suhl: small update

--- test_suhl/plot_suhl.m
+++ test_suhl/plot_suhl.m
@@ -5,7 +5,7 @@ addpath ../matlab
 [bs, as] = textread('2009_surovtsev.dat', '%f %f',...
                   'commentstyle', 'shell');
 bs=bs*180/pi;
-ab=mean(as(2:4)./bs(2:4))
+ab=mean(as(2:4)./bs(2:4));
 
 %%%
   b=0:0.01:180;
@@ -32,7 +32,7 @@ ab=mean(as(2:4)./bs(2:4))
 
 %%%
 
-ab = pi/180 / sqrt(10)
+ab = pi/180 / sqrt(10);
 
 
 figure; clf; hold on;
@@ -47,12 +47,16 @@ xlabel('\beta, deg');
 xlabel('a(\beta)');
 legend('a11', 'a12-13', 'a23', 'a', 'picture from paper', 'location', 'southeast')
 
-t=0.12:0.002:0.26;
-p=20;
-f=250000;
 
-D = he3_diff_perp_xx(t,p,f);
-o0=2*pi*f;
+t=0.12:0.002:0.26;
+f1=823000;
+f2=553000;
+p=4;
+
+D1 = he3_diff_perp_xx(t,p,f1);
+D2 = he3_diff_perp_xx(t,p,f2);
+o01=2*pi*f1;
+o02=2*pi*f2;
 ob=2*pi*he3_nu_b(t,p);
 
 cperp=he3_swvel_per(p,t*he3_tc(p));
@@ -60,7 +64,8 @@ cpar=he3_swvel_par(p,t*he3_tc(p));
 mu=0.25; % 1-cperp.^2./cpar.^2
 
 figure; clf; hold on;
-semilogy(t, (D*o0^3)./(2*ob.^2.*cpar.^2.*mu*ab)  , 'b.-');
+semilogy(t, (D1*o01^3)./(2*ob.^2.*cpar.^2.*mu*ab)  , 'b-');
+semilogy(t, (D2*o02^3)./(2*ob.^2.*cpar.^2.*mu*ab)  , 'r-');
 xlabel('T/T_c')
 ylabel('\beta_{max}, deg')
-
+legend('4bar, 823kHz', '4bar, 553kHz')
