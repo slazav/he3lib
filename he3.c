c4f56c8d63f73f48cb983c88c13d5dfcb1f330b0
Vladislav Zavjalov <slazav@altlinux.org>
2014-03-16 00:42:53 +0200

    lib: add table outbut to the command line interface

--- he3.c
+++ he3.c
@@ -1,8 +1,86 @@
 #include <math.h>
+#include <string.h>
 #include "he3tab.h"
 
 /* This is a command line interface for the he3lib.
                                        slazav, 2014 */
+
+/* Print short error message and exit */
+void
+error(const char * err){
+  printf("Error: %s\n", err);
+  exit(1);
+}
+
+/* Parse range setting in form <v1>:<v2>:<step>
+   Return number of steps. */
+int
+parse_range(const char * str,
+      double * v1, double * v2, double *st){
+  const char *del=":";
+  char *tok;
+
+  tok = strtok((char*)str,  del);
+  if (tok) *v1 = atof(tok);
+  else error("bad parameter");
+
+  tok = strtok(NULL, del);
+  if (tok) *v2 = atof(tok);
+  else { *v2=*v1; *st=0; return 1;}
+
+  tok = strtok(NULL, del);
+  *st = tok? atof(tok):(*v2-*v1)/(20-1);
+
+  if (*v1<*v2) *st = fabs(*st);
+  else *st = -fabs(*st);
+
+  if (*st==0) error("zero step");
+
+  return (int)ceil(abs((*v2-*v1)/(*st)))+1;
+}
+
+void
+call_func(struct tab_t * F, char *argv[]){
+  const int MAXARG=5; /* max number of arguments */
+  int N=1;            /* table length */
+  int i,j;
+  /* arguments: first and last value, step, current value */
+  double A1[MAXARG], A2[MAXARG], dA[MAXARG], A[MAXARG];
+  double (*ff)() = F->func;
+
+  /* parse arguments */
+  for (j=0; j<F->narg; j++){
+    int n1 = parse_range(argv[j], &A1[j], &A2[j], &dA[j]);
+    if (N!=1 && n1!=N) error("different parameter dementions");
+    if (n1!=1) N=n1;
+    A[j] = atof(argv[j]);
+  }
+
+  /* print table header */
+  if (N>1){
+    printf("# %s\n", F->comm);
+    printf("# %s %s\n", F->args, F->name);
+  }
+
+  /* call the function */
+  for (i=0;i<N;i++){
+    for (j=0; j<F->narg; j++){
+      A[j] = A1[j] + i*dA[j];
+      if (N>1) printf("%e ", A[j]);
+    }
+    switch(F->narg){
+      case 0: printf("%e\n", ff()); break;
+      case 1: printf("%e\n", ff(&A[0])); break;
+      case 2: printf("%e\n", ff(&A[0],&A[1])); break;
+      case 3: printf("%e\n", ff(&A[0],&A[1],&A[2])); break;
+      case 4: printf("%e\n", ff(&A[0],&A[1],&A[2],&A[3])); break;
+      case 5: printf("%e\n", ff(&A[0],&A[1],&A[2],&A[3],&A[4])); break;
+      default:
+        error("Error: unsupported function prototype\n");
+    }
+  }
+}
+
 
 int
 main(int argc, char *argv[]){
@@ -15,13 +93,13 @@ main(int argc, char *argv[]){
     printf("### Command list:\n");
     /* constants */
     for (i=0; const_tab[i].name!=NULL ; i++){
-      struct tab_t F=const_tab[i];
-      printf("%-22s %s\n", F.name, F.comm);
+      struct tab_t *F = &const_tab[i];
+      printf("%-22s %s\n", F->name, F->comm);
     }
     /* functions */
     for (i=0; func_tab[i].name!=NULL ; i++){
-      struct tab_t F=func_tab[i];
-      printf("%s(%s)\t%s\n", F.name, F.args, F.comm);
+      struct tab_t * F = &func_tab[i];
+      printf("%s(%s)\t%s\n", F->name, F->args, F->comm);
     }
     exit(1);
   }
@@ -30,40 +108,23 @@ main(int argc, char *argv[]){
   cmd=argv[1];
   /* look for the command in a constant list */
   for (i=0; const_tab[i].name!=NULL ; i++){
-    struct tab_t F=const_tab[i];
-    if (strcmp(const_tab[i].name, cmd)==0){
-      printf("%e\n", *(double *)const_tab[i].func);
+    struct tab_t *F = &const_tab[i];
+    if (strcmp(F->name, cmd)==0){
+      printf("%e\n", *(double *)F->func);
       exit(0);
     }
   }
   /* look for the command in a function list */
   for (i=0; func_tab[i].name!=NULL ; i++){
-    struct tab_t F=func_tab[i];
-    double (*ff)() = F.func;
-    if (strcmp(F.name, cmd)==0){
-      double A[16]; /* large enough to keep all args*/
+    struct tab_t *F = &func_tab[i];
+    if (strcmp(F->name, cmd)==0){
       /* check number of arguments */
-      if (argc-2!=F.narg){
-        printf("%s %s -- %s\n", F.name, F.args, F.comm);
+      if (argc-2!=F->narg){
+        printf("# %s\n", F->comm);
+        printf("Usage: %s %s\n", F->name, F->args, F->comm);
         exit(1);
       }
-      /* convert arguments to real numbers */
-      for (j=0; j<F.narg; j++){
-        A[j] = atof(argv[2+j]);
-      }
-
-      /* call the function */
-      switch(F.narg){
-        case 0: printf("%e\n", ff()); break;
-        case 1: printf("%e\n", ff(&A[0])); break;
-        case 2: printf("%e\n", ff(&A[0],&A[1])); break;
-        case 3: printf("%e\n", ff(&A[0],&A[1],&A[2])); break;
-        case 4: printf("%e\n", ff(&A[0],&A[1],&A[2],&A[3])); break;
-        case 5: printf("%e\n", ff(&A[0],&A[1],&A[2],&A[3],&A[4])); break;
-        default:
-          printf("Error: unsupported function prototype\n");
-          exit(1);
-      }
+      call_func(F, argv+2);
       exit(0);
     }
   }
