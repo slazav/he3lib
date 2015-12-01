0ee625f5d2c2878816a14be26a5dc3f1e9af90a7
Vladislav Zavjalov <slazav@altlinux.org>
2015-12-01 21:22:29 +0000

    test_gap: better way to calculate b2 phase gap

--- test_gap/plot_b2gap1.m
+++ test_gap/plot_b2gap1.m
@@ -8,15 +8,20 @@ function plot_g1_int()
 %  * effective field we
 % This program calculates the minimum of the energy
 
+
   find_figure('b2gap');
   clf; hold on;
   p=0.523;     % tc=1mK, as in the paper
-  %f0a = he3_f0a(p);
-  f0a = -0.75; % as in the paper
+  f0a = he3_f0a(p);
+%  f0a = -0.696; % as in the paper
 
-  ttc=0.1;
+  ttc=0.9;
   gap=he3_gap(ttc,p);
 
+  xx=-1:0.1:1;
+  yy=-1:0.1:1;
+  zz = 0.2;
+  w0 = 0.1;
 
 %  x=0.001:0.001:0.999;
 %  ct=0:0.001:1;
@@ -24,8 +29,6 @@ function plot_g1_int()
 %  int = integrand(xx,yy, gap,gap*1.01,gap*0.99,ttc, 0.05, 1);
 %  surface(xx,yy,int,'EdgeColor','none');
 
-%  % simple test -- find magnetization for a fixed gap distortion A,B
-%  % as a function of effective field
 %  xx=0:0.01:3;
 %  w0=0.1; A=0.0052; B=-0.0110;
 %  for i=1:length(xx)
@@ -44,13 +47,13 @@ function plot_g1_int()
 %  plot(xxh, mag1,'r-');
 
 
-  w0=0.03:0.03:0.8;
+  w0=0.00:0.01:0.4;
   find_figure('b2gap 1');
   clf; hold on;
 
   res=calc(w0,f0a,p,ttc);
-  plot(res.w0,res.A, 'r.-')
-  plot(res.w0,res.B, 'b.-')
+  plot(res.w0,res.A, 'r-')
+  plot(res.w0,res.B, 'b-')
 
   % table for Fischer's thesis for comparison
   f_h  = 0.05:0.01:0.33;
@@ -60,33 +63,20 @@ function plot_g1_int()
   f_D2 = [997 996 995 993 991 989 987 985 982 ...
           979 976 972 968 964 959 954 949 943 ...
           936 930 922 914 905 894 883 870 855 837 814]*1e-3;
-  plot(f_h,f_D1-1, 'r--')
-  plot(f_h,f_D2-1, 'b--')
+  plot(f_h,f_D1-1, 'ro')
+  plot(f_h,f_D2-1, 'bo')
 end
 
 function res = calc(w0,f0a,p,ttc);
+ he3_gyro=2.037800e+04;
   gap=he3_gap(ttc,p);
   tc = he3_tc(p)*1e-3*const_kb; % erg units
   w0G = w0*gap*tc/he3_gyro/const_hbar; % G
-
   for i=1:length(w0)
     % initial values:
     we(i)=(1-4*f0a*he3_chi_b(ttc,p))*w0(i);
     A(i)=0.1; B(i)=-0.1;
-    vt=0;
-    if (vt==0) % minimize all three functions
-      func=@(pp) zerofunc(ttc, gap, f0a, w0(i), pp(1),pp(2),pp(3));
-      pp = fminsearch(func, [we(i), A(i), B(i)]);
-      we(i)=pp(1);
-      A(i)=pp(2);
-      B(i)=pp(3);
-    elseif (vt==1) % calculate spin polarization from known susceptibility
-      func=@(pp) zerofunc2(ttc, gap, f0a, w0(i), we(i), pp(1),pp(2));
-      pp = fminsearch(func, [A(i), B(i)]);
-      A(i)=pp(1);
-      B(i)=pp(2);
-    end
-    fprintf('%f %f  %f %f  %f\n', w0(i), w0G(i), A(i), B(i), we(i)/w0(i))
+    [we(i) A(i) B(i)] = find_min(w0(i),f0a,p,ttc);
   end
   res.w0=w0;
   res.w0G=w0G;
@@ -94,6 +84,23 @@ function res = calc(w0,f0a,p,ttc);
   res.B=B;
 end
 
+function [we A B] = find_min(w0,f0a,p,ttc);
+  gap=he3_gap(ttc,p);
+  pp=[w0,0,0];
+  for i=1:100
+    dpp = zerofunc(ttc, gap, f0a, w0, pp(1),pp(2), pp(3));
+    pp=pp+2*dpp;
+    if sum(dpp.^2)<1e-10; break; end
+    if abs(pp(2))>1 || abs(pp(3))>1; pp=[NaN NaN NaN]; break; end
+  end
+  fprintf('> %f %f %f\n', pp);
+  we=pp(1);
+  A=pp(2);
+  B=pp(3);
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% integrands for dF/dgap1^2, dF/dgap2^2, dF/we
 function int = integrand(x,ct,gap0,gap1,gap2,TTc,we, n)
   %c = 2;  % important power factor
@@ -111,7 +118,7 @@ function int = integrand(x,ct,gap0,gap1,gap2,TTc,we, n)
   AB = -1/4*tanh(EB/(2*TTc))./EB;
 
   if n==1;
-    int  = -(Ap.*(we/2-Ez) + Am.*(we/2+Ez));
+    int  = (Ap.*(we/2-Ez) + Am.*(we/2+Ez))/2;
   elseif n==2;
     int  = (Ap+Am-2*AB).*(1-ct.^2);
   elseif n==3;
@@ -122,6 +129,7 @@ function int = integrand(x,ct,gap0,gap1,gap2,TTc,we, n)
   int(find(isnan(int)))=0;
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% functions for minimization
 %% gap in tc units, w0,we,A,B in gap units!
 function v = zerofunc(ttc, gap, f0a, w0, we, A,B)
@@ -131,30 +139,9 @@ function v = zerofunc(ttc, gap, f0a, w0, we, A,B)
   f1 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,1);
   f2 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,2);
   f3 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,3);
-  v1 = quad2d(f1,0,1,0,1)/2 - (we-w0)/(4*f0a);
+  v1 = quad2d(f1,0,1,0,1) + (we-w0)/(4*f0a);
   v2 = quad2d(f2,0,1,0,1);
   v3 = quad2d(f3,0,1,0,1);
   %  fprintf('%f %f %f %f -- %f %f %f\n', w0, A, B, we, v1,v2,v3)
-  v = v1^2 + v2^2 + v3^2;
+  v = [v1 v2 v3];
 end
-
-function v = zerofunc2(ttc, gap, f0a, w0, we, A,B)
-  we=we*gap; w0=w0*gap;
-  gap1 = gap*(1+A);
-  gap2 = gap*(1+B);
-  f2 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,2);
-  f3 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,3);
-  v2 = quad2d(f2,0,1,0,1);
-  v3 = quad2d(f3,0,1,0,1);
-  v=v2^2+v3^2;
-end
-
-function v = zerofunc1(ttc, gap, f0a, w0, we, A,B)
-  we=we*gap; w0=w0*gap;
-  gap1 = gap*(1+A);
-  gap2 = gap*(1+B);
-  f1 = @(x,y) integrand(x,y, gap,gap1,gap2,ttc,we,1);
-  v1 = quad2d(f1,0,1,0,1);
-  v=v1;
-end
-
