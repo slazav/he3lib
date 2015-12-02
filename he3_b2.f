c2f2b9b987a52031438a22adf7710bf1c58c9675
Vladislav Zavjalov <slazav@altlinux.org>
2015-12-02 18:18:24 +0000

    he3_b2: add He3_b2hcr(ttc,p) function for AB transition field

--- he3_b2.f
+++ he3_b2.f
@@ -1,5 +1,72 @@
 ! B phase in a strong magnetic field (B2 phase).
 
+!! B_ab [mk] vs P, ttc
+! Inseob Hahn PhD thesis, p79
+! see also code at http://spindry.phys.northwestern.edu/he3.htm
+      function pfunc(p,a,n)
+      ! function used to approximate pressure-dependent parameters
+        real*8 p,a(n),pfunc
+        integer i
+        pfunc=0
+        do i=1,n-1
+          pfunc = pfunc + a(i)*p**(i-1)
+        enddo
+        pfunc = pfunc/(1D0+a(n)*p)
+      end
+      function He3_b2hcr(ttc,P)
+        implicit none
+        include 'he3.fh'
+        real*8 P,ttc, tc,tab
+        real*8 p0,p1,p2,q1,pp
+        real*8 f1,f2,f3,f4,f5
+        real*8 Bc,Bo,gr, Pa, BB, X2
+        real*8 f0a,pfunc
+
+        ! values for pfunc()
+        real*8 coeff_Bc(4), coeff_Ta(4), coeff_f5(6)
+        data
+     .    coeff_Bc /3391D0, 21500D0, -8490D0, 2.098D0/,
+     .    coeff_Ta /1.5745D0,-1.1222D0,0.3242D0,0D0/,
+     .    coeff_f5 /0.041870D0, 5.417531D0, -10.044312D0,
+     .              7.639438D0, -2.379517D0, -0.537422D0/
+
+        Pa = 34.338
+        pp = P/Pa
+
+        f0a = he3_f0a(P)
+        Tc  = he3_tc(P)
+
+       ! Critical field at zero temperature.
+        Bc  = pfunc(pp, coeff_Bc, 4)
+
+       ! Tab/Tc. We need this value which is defined
+       ! in the whole pressure range and goes above 1 at low pressures.
+       ! This value is close to he3_tab(P)/he3_tc(P)
+        tab = pfunc(pp, coeff_Ta, 4)
+
+        f3 = 1.41D0
+        f4 = -0.29D0 -0.41D0*pp
+        gr = 0.616D0 - 1.174D0*pp + 0.301D0*pp**2
+
+        Bo = 19700 * Tc * (1+f0a)
+        BB = (Bo/Bc)**2 *gr/4D0
+        ! This is the original value of f5 which has problems near Tab=1
+        ! In expression A/BB both parts crosses 0 at this point.
+        ! And this crossing comes from different places (gr and tab definition)
+        !f5 = (1D0 - f3*Tab**6 - f4*Tab**8 - (1D0-f3-f4)*Tab**2
+     .  !      + (1D0+2D0*f3+3D0*f4)*(Tab**4-Tab**2))
+     .  !     /(BB * (Tab**4-Tab**2)) - 1D0
+        ! I will use the smooth approximation instead:
+        f5 = pfunc(pp, coeff_f5, 6)
+
+        f2 = BB * (1D0+f5) - (1D0+2D0*f3+3D0*f4)
+        f1 = 1D0+f5-f2-f3-f4
+
+        X2 = (f1*ttc**2 + f2*ttc**4 + f3*ttc**6 + f4*ttc**8)
+     .    / (1+f5*ttc**2)
+        he3_b2hcr = dsqrt(1D0 - X2**2) * Bc
+      end
+
 ! Calculation of B-phase gap distortion and spin polarization.
 ! Based on Ashida and Nagai paper (Progr.Theor.Phys. 74 949 (1985)).
 
@@ -97,7 +164,7 @@
         sl1 = 3D0 ! slope
         sl2 = 3D0 ! slope
         sl3 = 3D0 ! slope
-        do i=1,100
+        do i=1,1000
           dA  = sl1*F1a
           dB  = sl2*F2a
           dwe = sl3*F3a
@@ -105,21 +172,19 @@
           B   = B + dB
           we  = we + dwe
           call he3_b2_fder(ttc,gap,A,B,we,w0,f0a, F1b,F2b,F3b)
-          if ((abs(F1b)+abs(F2b)+abs(F3b)).lt.1D-10) goto 1
-          if (A.le.-1D0.or.B.le.-1D0) then
-            A=1D0/0D0
-            B=1D0/0D0
-            we=1D0/0D0
-            goto 1
-          endif
-          if (abs(dA/(F1a-F1b))<1D2)  sl1 = (dA/(F1a-F1b))
-          if (abs(dB/(F2a-F2b))<1D2)  sl2 = (dB/(F2a-F2b))
-          if (abs(dwe/(F3a-F3b))<1D2) sl3 = (dwe/(F3a-F3b))
+          if ((abs(F1b)+abs(F2b)+abs(F3b)).lt.1D-10) goto 2
+          if (A.le.-1D0.or.B.le.-1D0) goto 1
+          if (abs(dA/(F1a-F1b))<1D2)  sl1 = (sl1 + dA/(F1a-F1b))/2D0
+          if (abs(dB/(F2a-F2b))<1D2)  sl2 = (sl1 + dB/(F2a-F2b))/2D0
+          if (abs(dwe/(F3a-F3b))<1D2) sl3 = (sl1 + dwe/(F3a-F3b))/2D0
           F1a=F1b
           F2a=F2b
           F3a=F3b
         enddo
-1       gap1=gap*(1D0+A)
+1       A=1D0/0D0
+        B=1D0/0D0
+        we=1D0/0D0
+2       gap1=gap*(1D0+A)
         gap2=gap*(1D0+B)
         He = we*tc/(he3_gyro*const_hbar) ! Tc units -> G
       end
