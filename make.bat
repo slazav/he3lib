f004d0a2a28b46d87840818238d16cbf73697aa5
Vladislav Zavjalov <slazav@altlinux.org>
2020-03-22 14:12:57 +0200

    fix make.bat script

--- make.bat
+++ make.bat
@@ -3,5 +3,6 @@
 # it's a simplies script to build a command line program. For those who
 # do not want to adjust the huge and strange Makefile.
 
+./make_inc
 gcc -c he3.c -o he3.o
 gfortran -fno-range-check -O -std=legacy *.f he3.o -o he3
