291ec0367708aaad650998d98f43e9e115f55795
Vladislav Zavjalov <slazav@altlinux.org>
2020-03-25 17:18:33 +0200

    he3_const.f: add some hydrogen constants

--- functions/he3_const.f
+++ functions/he3_const.f
@@ -24,11 +24,16 @@
      .    const_mu0  /1.2566370614D0/,  !C> vacuum permeability [G*cm/A]
      .    const_ev   /1.602176634D-12/, !C> electronvolt [erg]
 
-     .    he3_gyro   /20378.0D0/,    !C> He3 g-factor, [1/s/G]
-     .    he3_amass  /5.0079D-24/,   !C> He3 atom mass, [g]
-     .    he3_mmass  /3.0158281D0/,  !C> He3 molar mass, [g/mol]
+     .    h1_gyro    /26752.218744D0/,  !C> H1 g-factor, [rad/s/G]
+     .    h2_gyro    /4106.5D0/,        !C> H2 g-factor, [rad/s/G]
+     .    h_amass    /1.67355755D-24/,  !C> H molar mass, [g/mol]
+     .    h_mmass    /1.00784D0/        !C> H molar mass, [g/mol]
 
-     .    he4_amass  /6.6465D-24/,   !C> He4 atom mass, [g]
-     .    he4_mmass  /4.002602D0/    !C> He4 molar mass, [g/mol]
+     .    he3_gyro   /20378.9D0/,       !C> He3 g-factor, [rad/s/G]
+     .    he3_amass  /5.00789994D-24/,  !C> He3 atom mass, [g]
+     .    he3_mmass  /3.0158281D0/,     !C> He3 molar mass, [g/mol]
+
+     .    he4_amass  /6.64647641D-24/,  !C> He4 atom mass, [g]
+     .    he4_mmass  /4.002602D0/       !C> He4 molar mass, [g/mol]
 
       end
