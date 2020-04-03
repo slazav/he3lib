a91b2ed157ede3f3d01d7113fd5f99fd7d0fb726
Vladislav Zavjalov <slazav@altlinux.org>
2020-04-03 20:24:43 +0100

    update make.bat

--- make.bat
+++ make.bat
@@ -5,4 +5,12 @@
 
 ./make_inc
 gcc -c he3.c -o he3.o
-gfortran -fno-range-check -O -std=legacy *.f he3.o -o he3
+gfortran -fno-range-check -O -I. -std=legacy functions/*.f he3.o -o he3
+gfortran -fno-range-check -O -I. -std=legacy --shared -fPIC functions/*.f -o he3.dll
+
+## matlab -- not tested.
+## -rpath should contain folder where .so/.dll file is located (if is is not
+##  a standard system path for libraries)
+
+#matlab64 -r 'mex -output he3lib.mex -L. LDFLAGS="-Wl,-rpath=." -lhe3 he3lib_mex.c'
+
